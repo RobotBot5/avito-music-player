55037043309aa36a5cefb4269d4d8aa68270a50d
Ilya Kazakov <robotbotyourube@gmail.com>
2025-02-12 02:03:56 +0300

    Remove unnecessary import in MusicRepository

--- features/music-api/src/main/java/com/robotbot/avito/music_api/domain/repositores/MusicRepository.kt
+++ features/music-api/src/main/java/com/robotbot/avito/music_api/domain/repositores/MusicRepository.kt
@@ -1,7 +1,6 @@
 package com.robotbot.avito.music_api.domain.repositores
 
 import com.robotbot.avito.music_api.domain.entities.Song
-import kotlinx.coroutines.flow.Flow
 
 interface MusicRepository {
 
